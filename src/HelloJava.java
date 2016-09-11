0880ea3145c58dcd87a04a0df23063b86799331c
writersurya <writer.suryarao@gmail.com>
2016-09-11 12:10:30 -0500

    hello world!

---
+++ src/HelloJava.java
@@ -0,0 +1,26 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		String name = "Surya";
+		int age = 34;
+		boolean male = true;
+		boolean female = false;
+		String location = "Kansas City";
+		
+		System.out.println(name +", "+age +", "+male +", "+location);
+		
+		
+		if(male)
+		{
+			System.out.println(name +" is Male." );
+		}
+		if(female)
+		{
+			System.out.println(name +" is Female." );
+		}
+
+	}
+
+}
